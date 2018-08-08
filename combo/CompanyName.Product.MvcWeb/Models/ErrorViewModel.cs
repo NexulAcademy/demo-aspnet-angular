cb3def2f147edeeebe4e78f53b2a4ebf87a4a9df
Michael Lang <michael.lang@nexul.com>
2018-08-08 15:57:51 -0500

    Step101: dotnet new mvc -o CompanyName.Product.MvcWeb

---
+++ combo/CompanyName.Product.MvcWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CompanyName.Product.MvcWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
